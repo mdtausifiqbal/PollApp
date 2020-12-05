5e4bd76475daf7af1f96bfe501d31bf584a7b6a3
Md Tausif Iqbal <mdtausifiqbalnwd@gmail.com>
2020-12-05 15:30:54 +0530

    customized admin panel

--- polls/admin.py
+++ polls/admin.py
@@ -2,5 +2,19 @@ from django.contrib import admin
 
 # Register your models here.
 from .models import Question, Choice
-admin.site.register(Question)
-admin.site.register(Choice)
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 0
+
+
+class QuestionAdmin(admin.ModelAdmin):
+    list_display = ('question_text', 'pub_date', 'was_published_recently')
+    list_filter = ['pub_date']
+    search_fields = ['question_text']
+    fieldsets = [
+        (None,               {'fields': ['question_text']}),
+        ('Date information', {'fields': ['pub_date']}),
+    ]
+    inlines = [ChoiceInline]
+
+admin.site.register(Question, QuestionAdmin)
